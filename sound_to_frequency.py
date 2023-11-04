20c4cf44098b5a5274b639080b08709e20e6409e
mLapatrie <maxencelapatrie@gmail.com>
2023-11-04 11:04:18 -0400

    update

--- sound_to_frequency.py
+++ sound_to_frequency.py
@@ -2,11 +2,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pywt
 import librosa
+import os
+
+
+def stimuli_to_bins(num_bins):
+    stimuli = os.listdir("Dataset/stimuli")
+    left_stimuli = []
+    for stim in stimuli:
+        if "Loc1" in stim or "Loc2" in stim:
+            left_stimuli.append(stim)
+    
+    bin_stim_edges = []
+    for sub in range(1, 7):
+        sub_stim_amplitudes = []
+        sub_stim_names = []
+        for stim in left_stimuli:
+            if f"sub0{sub}" in stim:
+                sub_stim_names.append(stim[:-4])
+                bin_edges, total_amplitudes = wav_to_bins(f"Dataset/stimuli/stim", num_bins)
+                if len(bin_stim_edges) == 0:
+                    bin_stim_edges = bin_edges
+                sub_stim_amplitudes.append(total_amplitudes)
+           
+        sub_stims = np.array(sub_stim_names)
+            
+        np.save(f"parsed_input_data/sub0{sub}_stims.npy", sub_stims)
+        np.save(f"parsed_input_data/sub0{sub}_stim_amplitudes.npy", sub_stim_amplitudes)
+        print(f"DONE Subject 0{sub}")
+    
+    print("Saving bin edges")
+    np.save(f"parsed_input_data/bin_edges.npy", bin_stim_edges)
 
 
 def wav_to_bins(filename, num_bins):
     # Replace 'your_audio_file.wav' with the path to your audio file
-    filename = 'sounds\sub01_Tool9_Loc2_ramp10.wav'
+    filename = 'Dataset/stimuli/sub01_Tool9_Loc2_ramp10.wav'
 
     # Load the audio file with librosa
     samples, sample_rate = librosa.load(filename, sr=None)
@@ -16,7 +46,6 @@ def wav_to_bins(filename, num_bins):
     coefficients, frequencies = pywt.cwt(samples, scales, 'morl', 1.0 / sample_rate)
 
     # Define logarithmically spaced frequency bins
-    num_bins = 16  # You can choose the number of bins based on your requirements
     bin_edges = np.logspace(np.log10(frequencies[1]), np.log10(frequencies[-1]), num_bins + 1)
 
     # Initialize an array to hold the total amplitude for each bin
@@ -26,7 +55,6 @@ def wav_to_bins(filename, num_bins):
     for i in range(num_bins):
         x = zip(coefficients, frequencies)
         for pair in x:
-            print(bin_edges[i+1], pair[1], bin_edges[i])
             if bin_edges[i+1] <= pair[1] < bin_edges[i]:
                 total_amplitudes[i] += np.sum(abs(pair[0]))
                 
